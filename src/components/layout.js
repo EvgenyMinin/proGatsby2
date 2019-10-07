3055178af3a61c03eae375c5fe9dd84d9fd2f30f
Johnny1992 <evgeny.frontend@gmail.com>
2019-10-07 10:49:35 +0300

    feature GraphQL Static Queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -18,6 +19,7 @@ const Layout = ({ children }) => {
       site {
         siteMetadata {
           title
+          description
         }
       }
     }
@@ -25,6 +27,18 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          {
+            name: 'description',
+            content: data.site.siteMetadata.description 
+          },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      >
+        <html lang='en' />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
